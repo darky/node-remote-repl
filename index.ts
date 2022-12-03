cb8e2e42902a20637bf6f5edc485e36784b7b672
Vladislav Botvin <darkvlados@gmail.com>
2022-12-03 22:06:34 +0300

    prettier

--- index.ts
+++ index.ts
@@ -1,64 +1,64 @@
-import path from "path";
-import fs from "fs";
+import path from 'path'
+import fs from 'fs'
 
-import { program } from "commander";
-import ts, { ModuleKind, ScriptTarget } from "typescript";
-import CRI from "chrome-remote-interface";
+import { program } from 'commander'
+import ts, { ModuleKind, ScriptTarget } from 'typescript'
+import CRI from 'chrome-remote-interface'
 
-import packageJson from "./package.json";
+import packageJson from './package.json'
 
 program
   .version(packageJson.version)
-  .name("node-remote-repl")
-  .usage("[options] <file>")
-  .option("-h, --host <string>", "host of inspect protocol", "localhost")
-  .option("-p, --port <number>", "port of inspect protocol", Number, 9229)
-  .parse(process.argv);
+  .name('node-remote-repl')
+  .usage('[options] <file>')
+  .option('-h, --host <string>', 'host of inspect protocol', 'localhost')
+  .option('-p, --port <number>', 'port of inspect protocol', Number, 9229)
+  .parse(process.argv)
 
-const file = program.args[0];
+const file = program.args[0]
 
 if (!file) {
-  console.error("Need to pass <file> argument");
-  process.exit(1);
+  console.error('Need to pass <file> argument')
+  process.exit(1)
 }
 
-const fileExtension = file.match(/(\.js|\.jsx|\.ts|\.tsx)$/)?.[1];
+const fileExtension = file.match(/(\.js|\.jsx|\.ts|\.tsx)$/)?.[1]
 
 if (!fileExtension) {
-  console.error("Support only js or ts file extensions");
-  process.exit(1);
+  console.error('Support only js or ts file extensions')
+  process.exit(1)
 }
 
-const filePath = path.resolve(process.cwd(), file);
+const filePath = path.resolve(process.cwd(), file)
 
 if (!fs.existsSync(filePath)) {
-  console.error(`File on path: "${filePath}" not exists`);
-  process.exit(1);
+  console.error(`File on path: "${filePath}" not exists`)
+  process.exit(1)
 }
 
-const fileContent = fs.readFileSync(filePath, { encoding: "utf8" });
+const fileContent = fs.readFileSync(filePath, { encoding: 'utf8' })
 const expression =
   fileExtension === '.js'
     ? fileContent
     : ts.transpileModule(fileContent, {
         compilerOptions: {
-          lib: ["es2020"],
+          lib: ['es2020'],
           module: ModuleKind.CommonJS,
           target: ScriptTarget.ES2020,
           esModuleInterop: true,
           jsx: fileExtension.endsWith('x') ? ts.JsxEmit.React : void 0,
         },
-      }).outputText;
+      }).outputText
 
-(async () => {
+;(async () => {
   type ProgramT = {
-    host: string;
-    port: number;
-  };
+    host: string
+    port: number
+  }
   const client = await CRI({
     host: program.opts<ProgramT>().host,
     port: program.opts<ProgramT>().port,
-  });
+  })
   const resp = await client.Runtime.evaluate({
     expression: `(async () => {
       const exports = {};
@@ -72,10 +72,10 @@ const expression =
     })()`,
     includeCommandLineAPI: true,
     awaitPromise: true,
-  });
-  console.log(resp.result.value || resp);
-  client.close();
+  })
+  console.log(resp.result.value || resp)
+  client.close()
 })().catch((e: Error) => {
-  console.error(e);
-  process.exit(1);
-});
+  console.error(e)
+  process.exit(1)
+})
